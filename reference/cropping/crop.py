cca924504549faf6e6264971fcde6b5930acdce2
Henrique Poleselo <hpoleselo@gmail.com>
2022-08-03 19:32:35 -0300

    Added crop without the data API to retrieve the json file.

--- reference/cropping/crop.py
+++ reference/cropping/crop.py
@@ -5,7 +5,7 @@ from sys import exit
 if __name__ == "__main__":
 
     print("\n----- Load a ply point cloud, print it, and render it -----")
-    pcd = o3d.io.read_point_cloud("../../TestData/fragment.ply")
+    pcd = o3d.io.read_point_cloud("./fragment.ply")
     if pcd.is_empty():
         exit()
     print(pcd)
@@ -17,6 +17,20 @@ if __name__ == "__main__":
     o3d.visualization.draw_geometries([downpcd])
 
     print("----- Load a polygon volume and use it to crop the original point cloud -----")
+    print("Load a polygon volume and use it to crop the original point cloud")
+
+    # Only works in open3d==0.15.0, which is available in newest pip version
+    """demo_crop_data = o3d.data.DemoCropPointCloud()
+    pcd = o3d.io.read_point_cloud(demo_crop_data.point_cloud_path)
+    vol = o3d.visualization.read_selection_polygon_volume(demo_crop_data.cropped_json_path)
+    chair = vol.crop_point_cloud(pcd)
+    o3d.visualization.draw_geometries([chair],
+                                    zoom=0.7,
+                                    front=[0.5439, -0.2333, -0.8060],
+                                    lookat=[2.4615, 2.1331, 1.338],
+                                    up=[-0.1781, -0.9708, 0.1608])"""
+
+
     vol = o3d.visualization.read_selection_polygon_volume(
         "../cropped.json")
     chair = vol.crop_point_cloud(pcd)
