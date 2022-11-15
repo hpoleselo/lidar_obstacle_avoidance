315bc884ce7155b44a2f9ef05674a6d7864e93e3
Henrique Poleselo <hpoleselo@gmail.com>
2022-11-15 08:53:56 -0300

    Added function to separate and paint clusters identified from HDBSCAN.

--- husky_with_livox/src/point_cloud_utils/offline_pcl_utils.py
+++ husky_with_livox/src/point_cloud_utils/offline_pcl_utils.py
@@ -4,6 +4,22 @@ import os
 import numpy as np
 import point_cloud_utils
 import os
+from pydantic import BaseModel
+from typing import List
+from collections import deque
+import random
+
+def visualize_pcd(pcd: list):
+    """
+    Visualize it in Open3D interface.
+    pcd: list. Must be passed in a list format, if wished multiple
+    point clouds can be passe.  
+    """
+    o3d.visualization.draw_geometries(pcd,
+                                    zoom=0.49,
+                                    front=[-0.4999, -0.1659, -0.8499],
+                                    lookat=[2.1813, 2.0619, 2.0999],
+                                    up=[0.1204, -0.9852, 0.1215])
 
 def concatenate_multiple_pcds(list_of_pcd):
     """
@@ -59,28 +75,98 @@ def read_point_cloud_files(test_data_path: list, directories: list) -> list:
     except ValueError as e:
         print(f"{e} due to wrong or non-existing file name.")
 
-def read_and_merge_pcd():
+def read_and_merge_pcd(test_data_path: str,
+                       point_cloud_directories: str
+                       ):
     # Adapting for local testing (i.e. reading the PCL as a file, not from ROS)
-    test_data_path = "../../test_data/"
-    point_clouds_directories = ["table_closer", "table_far_away", "wall_table_chair"]
-    list_of_files_path = read_point_cloud_files(test_data_path, point_clouds_directories)
+    list_of_files_path = read_point_cloud_files(test_data_path, point_cloud_directories)
     concatenated_pcd_path = concatenate_multiple_pcds(list_of_files_path)
     print(f"Reading: {concatenated_pcd_path}")
     pcd = o3d.io.read_point_cloud(concatenated_pcd_path)
     return pcd
 
+class ColoredPointCloudCluster(BaseModel):
+    """
+    Class that describes data structure to represent a cluster to be colored.
+    HDBSCAN will output N clusters, therefore N classes should be created 
+    """
+    label: int
+    color_open3d: List[float]
+    data_points: List[float]
+
+def get_number_of_labels(labels: list) -> list:
+    """
+    HDBSCAN will return an array containing the labels, this function creates a unique list out of
+    the unique ids.
+    """
+    return set(labels)
+
+def get_pc_size(pcd) -> int:
+    print(pcd)
+    print(type(pcd))
+    return pcd.shape[0]
+
+def generate_colored_clusters(unique_labels: list) -> deque:
+    """
+    Based on the N unique labels identified from HDBSCAN clustering algorithm, N classes
+    will be generated (along with colors) to be populated with the separeted points.
+    In other words N different point clouds will be generated and then merged together.
+    """
+
+    clusters = deque()
+    for label in unique_labels:
+        # Generates Red, Green and Blue color channels for the Cluster color
+        cpcc = ColoredPointCloudCluster(
+            label = label,
+            color_open3d = [
+                            round(random.uniform(0.0, 1.0), 3),    # Red channel
+                            round(random.uniform(0.0, 1.0), 3),    # Green channel
+                            round(random.uniform(0.0, 1.0), 3)],   # Blue channel
+            data_points = []
+        )
+        clusters.append(cpcc)
+    return clusters
+
+def assign_data_points_to_colored_cluster(
+                                  colored_clusters: List[ColoredPointCloudCluster],
+                                  point_cloud,
+                                  size_of_pc: int,
+                                  unique_labels: List[int], 
+                                  labels_from_hdbscan: List[List[float]]
+                                  ):
+    """
+    Compares each data point XYZ and its respective label from the point cloud and HDBSCAN output and
+    assigns each data point to the correct cluster (to the correct Class).
+    Each class (cluster) will be populated with their respective datapoints, other values are going to be 0. 
+    """
+    for i in range(size_of_pc):
+        for label in unique_labels:
+            if labels_from_hdbscan[i] == label:
+                # Since we're expecting the labels to be ordered, we can reference the correct class/cluster id by the label itself
+                # Stores the datapoint to the class
+                colored_clusters[label].data_points.append(point_cloud[i])
+
+def convert_to_open3d_and_paint_clusters(colored_point_cloud_clusters) -> deque:
+    colored_clusters_with_data_points = deque()
+    for colored_point_cloud_cluster in colored_point_cloud_clusters:
+        colored_pc = o3d.geometry.PointCloud()
+        colored_pc.points = o3d.utility.Vector3dVector(colored_point_cloud_cluster.data_points)
+        colored_pc.paint_uniform_color(colored_point_cloud_cluster.color_open3d)
+        colored_clusters_with_data_points.append(colored_pc)
+    return colored_clusters_with_data_points
+
 # Testing Locally
 if __name__ == '__main__':
-    pcd = read_and_merge_pcd()
+    test_data_path = "../../test_data/"
+    point_clouds_directories = ["table_closer", "table_far_away", "wall_table_chair"]
+    pcd = read_and_merge_pcd(test_data_path, point_clouds_directories)
 
     # To get min and max to perform the pass-through filter
     pcd_points = np.asarray(pcd.points)
 
     # Gets min and max from Point Cloud so that we just apply
     # Filter to the Z-Axis and other are wide enough
-    print(min(pcd_points[0]), min(pcd_points[1]), min(pcd_points[2]))
-    #print(min(pcd_points[0]))
-    #print(max(pcd_points[0]))
+    #print(min(pcd_points[0]), min(pcd_points[1]), min(pcd_points[2]))
     # Sensor is at least 0.3m above the ground
     #x_range = [-3.5, 3.5]
     #y_range = [-3.5, 3.5]
@@ -100,32 +186,16 @@ if __name__ == '__main__':
     downsampled_pcd = point_cloud_utils.downsample(pcd)
 
     segmented_pc = point_cloud_utils.segment_plane(downsampled_pcd)
-    point_cloud_utils.clustering_dbscan(segmented_pc)
 
-    
-    """new_col, new_pos = point_cloud_utils.draw_guild_lines(filter_boundaries)
-    new_data = np.concatenate((new_pos, new_col), axis = 1)
-    print(new_col)
-    print(new_pos)
-neno vai ficar no foco aq
-    guild_points = o3d.geometry.PointCloud()
-    guild_points.points = o3d.utility.Vector3dVector(new_pos)
-    guild_points.colors = o3d.utility.Vector3dVector(new_col)
+    # TODO: Test without segmenting the ground
+    cluster_labels_for_each_data_point = point_cloud_utils.cluster_hdbscan(segmented_pc)
 
-    o3d.visualization.draw_geometries([guild_points],
-                                    zoom=1,
-                                    front=[-0.4999, -0.1659, -0.8499],
-                                    lookat=[2.1813, 2.0619, 2.0999],
-                                    up=[0.1204, -0.9852, 0.1215])"""
+    pc = np.asarray(segmented_pc.points)
+    pc_size = get_pc_size(pc)
+    unique_labels = get_number_of_labels(cluster_labels_for_each_data_point)
+    colored_clusters = generate_colored_clusters(unique_labels)
+    assign_data_points_to_colored_cluster(colored_clusters, pc, pc_size, unique_labels, cluster_labels_for_each_data_point)
+    open3d_clusterized_colored_pc = convert_to_open3d_and_paint_clusters(colored_clusters)
+    visualize_pcd(open3d_clusterized_colored_pc)
 
-
-    """filtered_pcl = point_cloud_utils.pass_through_filter(filter_boundaries, pcd)
-
-    o3d.visualization.draw_geometries([filtered_pcl],
-                                    zoom=1,
-                                    front=[-0.4999, -0.1659, -0.8499],
-                                    lookat=[2.1813, 2.0619, 2.0999],
-                                    up=[0.1204, -0.9852, 0.1215])"""
-
-
-    #filtered_pcl = point_cloud_utils.apply_pass_through_filter(pcd, x_range, y_range, z_range)
+    #point_cloud_utils.clustering_dbscan(segmented_pc)
--- husky_with_livox/src/point_cloud_utils/point_cloud_utils.py
+++ husky_with_livox/src/point_cloud_utils/point_cloud_utils.py
@@ -210,28 +210,12 @@ def segment_plane(pcd):
                                     up=[0.1204, -0.9852, 0.1215])"""
     return outlier_cloud
 
-def convert_label_to_color():
-    pass
-
-def paint_each_cluster():
-    """ 
-    Separetes each identified cluster into a new list
-    
-    """
-    # Gets list of clusters
-    # 
-
 def cluster_hdbscan(pcd):
-
-    #o3dpc = copy.deepcopy(pcd)
-
-    #print(dir(pcd))
     pcd_points = np.asarray(pcd.points)
     print(pcd_points.shape)
     clusterer = hdbscan.HDBSCAN()
-    print(dir(clusterer))
+    #print(dir(clusterer))
     clusterer.fit(pcd_points)
-    print(f"Best epsilon found {clusterer.cluster_selection_epsilon()}")
     print(f"Got: {max(clusterer.labels_)+1} clusters.")
     print(f"List of labels: {clusterer.labels_}")
     # Use the Class to insert the label points and color them accordingly
@@ -244,31 +228,8 @@ def cluster_hdbscan(pcd):
     #                                min_cluster_size=2
     #                                )
 
-    # Convert labels array to color (Open3D understands colors between 0 and 1, not 0 and 255.)
-    converted_labels = deque()
-    for label in clusterer.labels_:
-        if label == 1:
-            converted_labels.append(label/5)
-        elif label == 0:
-            converted_labels.append(label)
-        elif label == 2:
-            converted_labels.append(label/2)
 
-    # TODO: Force a (n,3) conversion in order to match 
-    c2 = converted_labels.copy()
-    c3 = converted_labels.copy()
-
-    labels = [converted_labels, c2, c3]
-    
-    # Conversion to numpy is done in the end as it's more efficient
-    # and numpy array doesn't scale dynamically
-    # which means that we don't have to reallocate memory for the whole container once new elements appear.
-    colors = np.array(labels).transpose()
-    print(colors.shape)
-
-    pcd.colors = o3d.utility.Vector3dVector(colors)
-
-    return pcd
+    return clusterer.labels_
 
 # Testing Locally
 if __name__ == '__main__':
