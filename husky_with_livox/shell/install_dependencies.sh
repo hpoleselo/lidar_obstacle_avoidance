51d3658c52d69ab9039ef34f2f23369eb00b5b28
Henrique Poleselo <hpoleselo@gmail.com>
2023-04-05 00:25:40 -0300

    Replaced ROS version to env. variable and added other dependencies.

--- husky_with_livox/shell/install_dependencies.sh
+++ husky_with_livox/shell/install_dependencies.sh
@@ -1,3 +1,6 @@
+# Makes sure to source ROS, otherwise $ROS_DISTRO won't work below
+source source /opt/ros/noetic/setup.bash
+
 # Setup to clone livox repository
 sudo apt-get install git
 cd ~/catkin_ws/src
@@ -5,13 +8,19 @@ cd ~/catkin_ws/src
 # Clones Livox 
 git clone git@github.com:Livox-SDK/livox_laser_simulation.git
 
-# Make sure to have teleop installed
-sudo apt-get install ros-noetic-teleop-twist-keyboard
+# Clones LaR environment
+git clone https://github.com/lar-deeufba/lar_gazebo.git
+
+# Install Ignition dependencies
+sudo apt-get install libignition-math4*
 
 # Install all packages related to Husky
-sudo apt-get install ros-noetic-husky-gazebo
-sudo apt-get install ros-noetic-husky-control
-sudo apt-get install ros-noetic-husky-description
-sudo apt-get install ros-noetic-husky-msgs
-sudo apt-get install ros-noetic-husky-simulator
-sudo apt-get install ros-noetic-husky-viz
+sudo apt-get install ros-${ROS_DISTRO}-husky-gazebo
+sudo apt-get install ros-${ROS_DISTRO}-husky-control
+sudo apt-get install ros-${ROS_DISTRO}-husky-description
+sudo apt-get install ros-${ROS_DISTRO}-husky-msgs
+sudo apt-get install ros-${ROS_DISTRO}-husky-simulator
+sudo apt-get install ros-${ROS_DISTRO}-husky-viz
+
+cd ~/catkin_ws
+catkin_make
